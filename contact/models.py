7552af3971865a1314b5a6892209b190cdf0a9ed
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-01 10:36:52 -0300

    criação do primeiro módulo: contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    phone = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(timezone.now)
+    description = models.TextField(blank=True)
