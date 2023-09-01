40df182e35a7492a02fb72748672367ddb110f7f
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-01 11:36:11 -0300

    fazendo algumas configurações comuns de se usar na área administrativa do django

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,33 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    # configurações de exibição da área administratica do django
+    # lista os campos das tabelas do banco de dados
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+    )
+
+    # ordena os a exibição dos dados
+    ordering = (
+        '-id',
+    )
+
+    # faz buscas pelos campos definidos
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+
+    # quantidade de itens listados por página
+    list_per_page = 10
+
+    # quantidade de itens mostrados no "mostrar tudo"
+    list_max_show_all = 200
+
+    # deixa o campo de first name editável na área de pesquisa
+    # list_editable = 'first_name'
