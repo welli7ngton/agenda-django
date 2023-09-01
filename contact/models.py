6e23540ac1ae3037766f3c4586d02c26981f164b
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-01 11:17:19 -0300

    alterações nos campos do model

--- contact/models.py
+++ contact/models.py
@@ -6,6 +6,9 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
-    phone = models.EmailField(max_length=254, blank=True)
-    created_date = models.DateTimeField(timezone.now)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
