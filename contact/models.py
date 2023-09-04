4c952d1793de86263400e3ff74734fc7ada86a97
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-04 13:09:16 -0300

    migrações das alterações no meta options no model category e criação de owner no model contato

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -20,6 +25,13 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
+    owner = models.ForeignKey(
+        User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True
