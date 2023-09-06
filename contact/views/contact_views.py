6cb1680d0215a2c9d916123d96702cdc6b342f56
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-06 11:05:25 -0300

    criando view para contato unico

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -15,4 +15,17 @@ def index(request):
         request,
         'contact/index.html',
         context,
+    )
+
+
+def contact(request, contact_id):
+
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    return render(
+        request,
+        'contact/contact.html',
+        context={'contact': single_contact},
     )
