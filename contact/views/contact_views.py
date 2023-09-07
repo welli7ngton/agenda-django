b88b7b09049af27b7f0e7ae0384163497d6dc036
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-07 15:21:29 -0300

    fazendo paginação da agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # consulta sql do comanto acima
     # print(contacts.query)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
     return render(
@@ -50,11 +55,15 @@ def search(request):
         Q(email__icontains=search_value)
         ).order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     # consulta sql do comanto acima
     # print(contacts.query)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
