d1a76912a9637daf4907a47c6c2187fdf638a332
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-08 12:25:56 -0300

    criando primeira parte do formulario e alterando view para alternar entre action GET e POST

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django import forms
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone'
+        )
 
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+
+    context = {
+        'form': ContactForm()
     }
+
     return render(
         request,
         'contact/create.html',
