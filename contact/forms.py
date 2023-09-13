6838b5ed646251ea81e3c1b5f2561c1b68ec5784
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-13 10:30:03 -0300

    criando as validações e lógica para alteração dos dados sensíveis do usuário

--- contact/forms.py
+++ contact/forms.py
@@ -148,6 +148,33 @@ class RegisterUpdateForm(forms.ModelForm):
             'username'
         )
 
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas não conferem')
+                )
+
+        return super().clean()
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         print(User.objects.filter(email=email), 'print')
@@ -162,3 +189,16 @@ class RegisterUpdateForm(forms.ModelForm):
                 )
 
         return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+
+        return password1
